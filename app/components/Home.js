872c56bac4ea245c09ea8d5dbcbb4abbf7309e9a
Graig Janssen <graig.janssen@gmail.com>
2016-12-30 17:49:27 -0500

    Add rough styles to Home component

--- app/components/Home.js
+++ app/components/Home.js
@@ -7,19 +7,23 @@ var Home = React.createClass({
   render: function () {
     return (
       <div className="home">
-        <div className="col-xs-12 text-center">
-          <h1 id="title">QuizMe</h1>
-          <p id="subtitle">A study tool that helps you study.</p>
+        <div className="row text-center">
+          <div className="col-xs-12">
+            <h1 id="title">QuizMe</h1>
+          </div>
+          <div className="col-xs-12 col-md-8 col-md-offset-2">
+            <p id="subtitle">Studying for stuff is hard.  Now it is less hard.  Use QuizMe to build your very own quiz thing.  Do the thing and feel smarter betterer.</p>
+          </div>
         </div>
         <div className="row">
           <div className="col-xs-12 col-md-4 col-md-offset-2">
             <Link to="/setup">
-              <button type="button" className="btn btn-lg btn-primary">New Quiz</button>
+              <button type="button" className="btn btn-lg btn-primary space">New Quiz</button>
             </Link>
           </div>
           <div className="col-xs-12 col-md-4">
           <Link to="/add">
-            <button type="button" className="btn btn-lg btn-success">Add Question</button>
+            <button type="button" className="btn btn-lg btn-success space">Add Question</button>
           </Link>
           </div>
         </div>
