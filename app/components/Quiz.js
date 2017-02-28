480c8cb3d48993f976517f72a02f5db7a8fbf321
Graig Janssen <graig.janssen@gmail.com>
2017-02-28 14:36:19 -0500

    Add status bar UI for quiz component

--- app/components/Quiz.js
+++ app/components/Quiz.js
@@ -1,9 +1,29 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
+var Link = require('react-router').Link;
 require('!style!css!sass!../styles/main.scss');
 
 var Answer = require('../modules/Answer');
 
+function QuizBar(props){
+  return (
+    <div className="quiz-bar">
+      <div className="row">
+        <div className="col-xs-3 col-md-2 col-md-offset-1 text-left">
+          <Link to="/setup">
+            <button className="btn btn-danger">Exit</button>
+          </Link>
+        </div>
+        <div className="col-xs-3 col-xs-offset-2 col-md-2 col-md-offset-4 text-left">
+          Correct: {props.correct}
+        </div>
+        <div className="col-xs-4 col-md-3 text-left">
+          Progress: {props.currentQuestion + 1}/{props.totalQuestions}
+        </div>
+      </div>
+    </div>
+  )
+}
 function Quiz(props){
   var question = props.questions[props.currentQuestion];
   var answers = question.answers.map(function(answer, index){
@@ -15,7 +35,11 @@ function Quiz(props){
     )
   })
   return (
-    <div>
+    <div className="quiz">
+      <QuizBar
+      correct={props.correct}
+      currentQuestion={props.currentQuestion}
+      totalQuestions={props.totalQuestions}/>
       <h3>{question.text}</h3>
       {answers}
     </div>
