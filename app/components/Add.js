6f74c117d36c28f9313f62d058c3675ca2c1ed0e
Graig Janssen <graig.janssen@gmail.com>
2017-03-05 15:19:16 -0500

    Add UI and basic styling for add question component

--- app/components/Add.js
+++ app/components/Add.js
@@ -1,10 +1,69 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
 require('!style!css!sass!../styles/main.scss');
 
 function Add(props){
   return (
-    <div>Add!</div>
+    <div className="add">
+      <h1 className="section-heading">Add New Question</h1>
+      <div className="row">
+        <div className="col-xs-12 col-sm-6">
+          <h2 className="subheading">Category</h2>
+          <select>
+            <option value="create new">Create New</option>
+            <option value="Category 1">Category 1</option>
+            <option value="Category 2">Category 2</option>
+            <option value="Category 3">Category 3</option>
+            <option value="Category 4">Category 4</option>
+            <option value="Category 5">Category 5</option>
+          </select>
+        </div>
+        <div className="col-xs-12 col-sm-6">
+          <h2 className="subheading">Difficulty</h2>
+          <form>
+            <input className="add-radio" type="radio" name="difficulty" value="easy"/><span className="diff-label">Easy</span>
+            <input className="add-radio" type="radio" name="difficulty" value="medium"/><span className="diff-label">Medium</span>
+            <input className="add-radio" type="radio" name="difficulty" value="hard"/><span className="diff-label">Hard</span>
+          </form>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-12">
+          <h2>Question Text</h2>
+        </div>
+      </div>
+      <div className="row space">
+        <div className="col-xs-12">
+          <textarea rows="3" cols="36"></textarea>
+        </div>
+      </div>
+      <div className="row space">
+        <div className="col-xs-12">
+          <h2>Answers</h2>
+          <h4><em>Provide at least 2 answers and select the correct one</em></h4>
+          <input className="add-radio" type="radio" name="correct"/>
+          <input className="answer-input" type="text"/><br/>
+          <input className="add-radio" type="radio" name="correct"/>
+          <input className="answer-input" type="text"/><br/>
+          <input className="add-radio" type="radio" name="correct"/>
+          <input className="answer-input" type="text"/><br/>
+          <input className="add-radio" type="radio" name="correct"/>
+          <input className="answer-input" type="text"/><br/>
+        </div>
+      </div>
+      <div className="row space">
+        <div className="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3">
+          <button className="btn btn-lg btn-success space">Add <i className="fa fa-plus"></i></button>
+        </div>
+      </div>
+    </div>
   )
 }
+
+Add.propTypes = {
+  canSubmit: PropTypes.bool.isRequired,
+  handleSubmit: PropTypes.func.isRequired
+};
+
 
 module.exports = Add;
--- app/containers/AddContainer.js
+++ app/containers/AddContainer.js
@@ -1,11 +1,31 @@
 var React = require('react');
 require('!style!css!sass!../styles/main.scss');
+
 var Add = require('../components/Add');
 
 var AddContainer = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+  getInitialState: function () {
+    return {
+      canSubmit: false,
+      questionData: {
+        text: '',
+        difficulty: '',
+        category: '',
+        answers: []
+      }
+    }
+  },
+  handleSubmit: function () {
+    console.log('handle submit damnit!');
+  },
   render: function () {
     return (
-      <Add />
+      <Add
+      canSubmit={this.state.canSubmit}
+      handleSubmit={this.state.handleSubmit}/>
     )
   }
 });
