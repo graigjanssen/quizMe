4bd12af897d2fb0ded78d92e97f8e7904bf0e9e1
Graig Janssen <graig.janssen@gmail.com>
2017-03-04 20:14:06 -0500

    Add dyanamic styling for results based on score

--- app/components/Results.js
+++ app/components/Results.js
@@ -3,14 +3,30 @@ require('!style!css!sass!../styles/main.scss');
 var Link = require('react-router').Link;
 
 function Results(props){
+  var score = props.location.state.score;
+  var rating, message;
+  if (score < 60){
+    rating = "bad", message = "Ouch! Better luck next time!";
+  } else if (score < 80){
+    rating = "okay", message = "Nice work, but there's room for improvement...";
+  } else if (score < 100){
+    rating = "good", message = "Very nice! Keep up the good work!";
+  } else if (score === 100){
+    rating = "good", message = "Perfect! You know your stuff!";
+  } else {
+    rating = "okay", message = "I don't know what's going on";
+  }
   return (
-    <div className="results text-center">
-      <h1>Quiz Complete</h1>
-      <h3>Your Score</h3>
-      <h1>{props.location.state.score}%</h1>
-      <Link to="/setup">
-        <button className="btn btn-lg btn-primary">Take Another Quiz</button>
-      </Link>
+    <div className="results text-center row">
+      <h1 className="heading col-xs-12">Quiz Complete</h1>
+      <h2>Your Score</h2>
+      <h1 className={"score col-xs-2 col-xs-offset-5 " + rating}>{score}%</h1>
+      <h3 className="message col-xs-12">{message}</h3>
+      <div className="col-xs-6 col-xs-offset-3">
+        <Link to="/setup">
+          <button className="btn btn-lg btn-primary">Take Another Quiz</button>
+        </Link>
+      </div>
     </div>
   )
 }
