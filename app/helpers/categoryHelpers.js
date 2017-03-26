a8f50fb8fae4b11a43ede37ae192cea9313e16c7
Graig Janssen <graig.janssen@gmail.com>
2017-03-26 18:04:39 -0400

    New build trying to resolve GET request issue in production

--- app/helpers/categoryHelpers.js
+++ app/helpers/categoryHelpers.js
@@ -3,7 +3,7 @@ var axios = require('axios');
 var helpers = {
   getAll: function() {
     console.log('axios go get categories...');
-    return axios.get('/api/questions/categories')
+    return axios.get('/api/questions/categories/')
     .then(function(response){
       console.log('axios response:', response.data);
       return response.data.categoryTotals;
