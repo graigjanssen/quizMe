e449417f5affe4d2c9ba884f41d5fb4f553b3ffa
Graig Janssen <graig.janssen@gmail.com>
2017-03-26 17:39:59 -0400

    Add logging for production debugging, app is loading but no data from api...

--- app/helpers/categoryHelpers.js
+++ app/helpers/categoryHelpers.js
@@ -2,8 +2,10 @@ var axios = require('axios');
 
 var helpers = {
   getAll: function() {
+    console.log('axios go get categories...');
     return axios.get('/api/questions/categories')
     .then(function(response){
+      console.log('axios response:', response.data);
       return response.data.categoryTotals;
     });
   }
